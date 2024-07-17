2d3e81b2e2d429d312c5e33f12c742fd611972da
aniket728 <aniketjain7891@gmail.com>
2024-07-17 22:41:54 +0530

    first_comit

---
+++ 01 React project/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Hi, Aniket, this project has done by Vite</h1>
+  )
+}
+
+export default App
---
+++ 01react_project/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Hello Aniket, this project is done by React</h1>
+  );
+}
+
+export default App;
